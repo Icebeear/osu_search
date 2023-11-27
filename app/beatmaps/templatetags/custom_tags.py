41b3fd6a22d4f7082aa8a725187c174385781c1a
dante <arham59ru@gmail.com>
2023-11-27 13:45:16 +0300

    some frontend fixes

--- app/beatmaps/templatetags/custom_tags.py
+++ app/beatmaps/templatetags/custom_tags.py
@@ -10,28 +10,47 @@ def format_time(seconds):
 
 @register.simple_tag
 def set_color(star):
-    if star >= 1 and star < 2:
+    if 1 <= star < 2:
         return "#6EFF79"
-    elif star >= 2 and star < 3:
+    elif 2 <= star < 3:
         return "#4FC0FF"
-    elif star >= 3 and star < 4:
+    elif 3 <= star < 4:
         return "#F8DA5E"
-    elif star >= 4 and star < 5:
+    elif 4 <= star < 5:
         return "#FF7F68"
-    elif star >= 5 and star < 6:
+    elif 5 <= star < 6:
         return "#FF4E6F"
-    elif star >= 6 and star < 7:
+    elif 6 <= star < 7:
         return "#A653B0"
-    elif star >= 7 and star < 8:
+    elif 7 <= star < 8:
         return "#3B38B2"
     else:
         return "#000000"
     
 @register.simple_tag
 def format_dif(star):
-    return round(star, 2)
-
+    return f"{star:.2f}"
 
 @register.simple_tag
 def format_labels(val):
-    return f"{val // 1000}k"
+    if val < 1000:
+        return val
+    else:
+        return f"{val // 1000}k"
+
+@register.simple_tag
+def bpm_round(val):
+    return int(val)
+
+@register.simple_tag
+def get_diff_id(link):
+    return link.split("/")[-1]
+
+@register.simple_tag
+def title_resize(title):
+    if len(title) > 30:
+        return title[:27] + "..."
+    else:
+        return title 
+
+#Yomi yori Kikoyu, Koukoku no Tou to Honoo no Shoujo.
